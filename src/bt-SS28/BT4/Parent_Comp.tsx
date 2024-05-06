f4fe33b41830cf5ee6f650b6fe5cfb58925c4b6a
minhhien165 <147390428+minhhien165@users.noreply.github.com>
2024-05-06 11:35:02 +0700

    Add files via upload

---
+++ src/bt-SS28/BT4/Parent_Comp.tsx
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import Children_Comp from './Children_Comp';
+interface ClassBT4 {
+    name:string
+}
+
+export default class Parent_Comp extends Component <any,ClassBT4>{
+  constructor(props:ClassBT4) {
+    super(props);
+    this.state = {
+      name: "Nguyễn Minh Hiển"
+    };
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Họ và tên bên component cha: {this.state.name}</h1>
+        <Children_Comp name={this.state.name} />
+      </div>
+    );
+  }
+}
